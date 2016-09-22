b9ff6ad2b2cac1cf74e0d671e46acda3960ffd78
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-21 19:22:01 -0700

    remove mock data

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,20 +1,7 @@
 import firebase from 'firebase';
 import { FETCH_PROJECTS } from './types';
 
-
-const database = app.database();
-const projects = database.ref('projects');
-
-// const INITIAL_PROJECTS = [{ id: 1, name: 'Tower' }, 
-//                           { id: 2, name: 'Apartment' },
-//                           { id: 3, name: 'Office' }];
-
-// export function fetchProjects() {
-//   return {
-//     type: FETCH_PROJECTS,
-//     payload: INITIAL_PROJECTS
-//   };
-// }
+const projects = firebaseDb.ref('projects');
 
 export function fetchProjects() {
   return dispatch => {
