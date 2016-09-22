505c737fd76602422eb4544a141cc23b45dd6a0d
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-21 17:07:47 -0700

    edit logout message

--- src/app/components/user/logout.jsx
+++ src/app/components/user/logout.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-
 export default () => {
   return (
     <form id="frmLogout" role="form">
-      <h2>You are logged out!</h2>
+      <h2>You have successfully logged out.</h2>
     </form>
   )
 }
