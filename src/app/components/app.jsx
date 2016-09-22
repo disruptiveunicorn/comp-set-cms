e254a66505d48224a8f61f2b8714c57cf0783b4f
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-21 17:02:52 -0700

    fix formatting

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {fetchUser, logoutUser}  from '../actions/firebase_actions';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchUser, logoutUser }  from '../actions/firebase_actions';
 
 class App extends Component {
 
