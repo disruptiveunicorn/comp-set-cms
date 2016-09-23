a9b17164e81e921414ccd38d9f52489f74d8c48e
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-09-22 19:40:50 -0700

    add auth logic to Projects

--- src/app/components/user/projects.jsx
+++ src/app/components/user/projects.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import firebase from '../../utils/firebase';
 import { fetchProjects } from '../../actions/index';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
+import Loading from '../helpers/loading';
+import { fetchUser } from '../../actions/firebase_actions';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.props.fetchUser();
+  }
+
   componentWillMount() {
     this.props.fetchProjects();
   }
@@ -19,9 +28,15 @@ class Projects extends Component {
   }
 
   render() {
+    if (!this.props.currentUser) {
+      return <Loading/>
+    }
+
     return (
       <div>
-        <h3>Projects</h3>
+        <h3>Projects
+          <Link to="/new_project" className="btn btn-default pull-right">New Project</Link>
+        </h3>
         <ul className='list-group'>
           {this.renderProjects()}
         </ul>
@@ -31,12 +46,13 @@ class Projects extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchProjects }, dispatch);
+  return bindActionCreators({ fetchProjects, fetchUser }, dispatch);
 }
 
 function mapStateToProps(state) {
   return {
-    projects: state.projects.all
+    projects: state.projects.all,
+    currentUser: state.currentUser
   }
 }
 
