e9226ec4a9ad418d6ae5832c136b8e17609d88ab
Chris Csekey <cse25@users.noreply.github.com>
2016-09-23 23:19:08 -0700

    remove firebase config

--- src/app/config.js
+++ src/app/config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  FIREBASE_CONFIG: {
-    apiKey: "AIzaSyBnvrvgUxjFYs2jRSG57clBzgpCrvX4IlI",
-    authDomain: "propertyviewer-5c429.firebaseapp.com",
-    databaseURL: "https://propertyviewer-5c429.firebaseio.com",
-    storageBucket: "propertyviewer-5c429.appspot.com"
-  }
-}
+// Put Firebase config here
